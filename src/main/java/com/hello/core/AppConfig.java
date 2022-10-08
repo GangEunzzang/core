43d7b3797072dfae187d76e22f8d6d7d0f2705e0
이강은 <rkddms123456@gmail.com>
2022-10-08 16:16:57 +0900

    AppConfig 리팩토링

--- src/main/java/com/hello/core/AppConfig.java
+++ src/main/java/com/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package com.hello.core;
 
+import com.hello.core.discount.DisCountPolicy;
 import com.hello.core.discount.FixDiscountPolicy;
+import com.hello.core.member.MemberRepository;
 import com.hello.core.member.MemberService;
 import com.hello.core.member.MemberServiceImpl;
 import com.hello.core.member.MemoryMemberRepository;
@@ -10,10 +12,18 @@ import com.hello.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private DisCountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
