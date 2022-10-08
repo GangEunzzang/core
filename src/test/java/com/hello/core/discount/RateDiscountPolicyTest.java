14e8fe34d8c05c5b5290277cb1d43a21ba13500d
이강은 <rkddms123456@gmail.com>
2022-10-08 15:23:06 +0900

    새로운 할인 정책 개발

---
+++ src/test/java/com/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,41 @@
+package com.hello.core.discount;
+
+import com.hello.core.member.Grade;
+import com.hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    public void  VIP_10프로_할인() throws Exception {
+
+        //given
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+
+        //then
+        assertThat(discount).isEqualTo(1000);
+    }
+
+
+    @Test
+    public void VIP_아니면_할인적용이_안됨 () throws Exception {
+
+        //given
+        Member member = new Member(2L, "memberVIP", Grade.BASIC);
+
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+
+        //then
+        assertThat(discount).isEqualTo(0);
+     }
+}
