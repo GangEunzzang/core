82d776f4ce4727ba1fb41b0c57286e314af983c6
이강은 <rkddms123456@gmail.com>
2022-10-09 21:56:24 +0900

    스프링으로 전환하기2

---
+++ src/test/java/com/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,35 @@
+package com.hello.core.beanfind;
+
+import com.hello.core.AppConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextInfoTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    void 모든빈_출력() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("beanDefinitionName = " + beanDefinitionName + "object = " + bean);
+
+        }
+    }
+
+
+    @Test
+    void 애플리케이션_빈_출력() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("beanDefinitionName = " + beanDefinitionName + "object = " + bean);
+            }
+        }
+    }
+}
