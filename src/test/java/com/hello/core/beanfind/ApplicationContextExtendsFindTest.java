65ebbbf6c9dc7a55add64c2a87263c1f397975dd
이강은 <rkddms123456@gmail.com>
2022-10-10 23:33:17 +0900

    스프링 빈 조회 테스트 케이스 작성

---
+++ src/test/java/com/hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -0,0 +1,52 @@
+package com.hello.core.beanfind;
+
+import com.hello.core.discount.DisCountPolicy;
+import com.hello.core.discount.FixDiscountPolicy;
+import com.hello.core.discount.RateDiscountPolicy;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ApplicationContextExtendsFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+
+
+    @Test
+    void 부모타입으로_조회시_자식이둘이상이면_중복오류가발생() {
+        Assertions.assertThrows(NoUniqueBeanDefinitionException.class,
+                () -> ac.getBean(DisCountPolicy.class));
+    }
+
+    @Test
+    void 부모타입으로조회시_자식이둘이상이면_빈이름을지정하면된다() {
+        DisCountPolicy rateDiscountPolicy = ac.getBean("rateDiscountPolicy", DisCountPolicy.class);
+        assertThat(rateDiscountPolicy).isInstanceOf(RateDiscountPolicy.class);
+    }
+
+    @Test
+    void 특정하위타입으로_조회() {
+        RateDiscountPolicy rateDiscountPolicy = ac.getBean(RateDiscountPolicy.class);
+        assertThat(rateDiscountPolicy).isInstanceOf(RateDiscountPolicy.class);
+    }
+
+
+    @Configuration
+    static class TestConfig {
+
+        @Bean
+        public DisCountPolicy rateDiscountPolicy() {
+            return new RateDiscountPolicy();
+        }
+
+        @Bean
+        public DisCountPolicy fixDiscountPolicy() {
+            return new FixDiscountPolicy();
+        }
+    }
+}
