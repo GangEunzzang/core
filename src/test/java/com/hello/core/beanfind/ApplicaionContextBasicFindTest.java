7399e862f05d14f8bac4c6a0cb409ae775b872e8
이강은 <rkddms123456@gmail.com>
2022-10-10 22:51:58 +0900

    스프링 빈 조회 테스트 케이스 작성

---
+++ src/test/java/com/hello/core/beanfind/ApplicaionContextBasicFindTest.java
@@ -0,0 +1,36 @@
+package com.hello.core.beanfind;
+
+import com.hello.core.AppConfig;
+import com.hello.core.member.MemberService;
+import com.hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ApplicaionContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    public void 빈이름으로_조회() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    public void 이름없이_타입으로_조회() {
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    public void 빈이름으로_조회가_안될때() {
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("xxxx", MemberService.class));
+    }
+
+}
