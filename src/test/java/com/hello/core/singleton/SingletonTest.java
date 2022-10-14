730c054e37e1b2b722f89c3b5b9f05e5d21369ce
이강은 <rkddms123456@gmail.com>
2022-10-15 01:16:21 +0900

    싱글톤 방식의 문제점

--- src/test/java/com/hello/core/singleton/SingletonTest.java
+++ src/test/java/com/hello/core/singleton/SingletonTest.java
@@ -4,6 +4,8 @@ import com.hello.core.AppConfig;
 import com.hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -25,6 +27,20 @@ public class SingletonTest {
         SingletonService singletonService2 = SingletonService.getInstance();
 
         assertThat(singletonService1).isSameAs(singletonService2);
-ㅁㄴ
+        
     }
+    
+    @Test
+    public void 스프링컨테이너와_싱글톤 () throws Exception {
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        assertThat(memberService1).isSameAs(memberService2);
+
+    }
+
+
 }
