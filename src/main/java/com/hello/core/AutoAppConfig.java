9471644992d5884d9e690afbad48775c52ef8e35
이강은 <rkddms123456@gmail.com>
2022-10-20 23:38:42 +0900

    컴포넌트 스캔

--- src/main/java/com/hello/core/AutoAppConfig.java
+++ src/main/java/com/hello/core/AutoAppConfig.java
@@ -1,9 +1,13 @@
 package com.hello.core;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @ComponentScan
 @Configuration
 public class AutoAppConfig {
+
+    @Bean
+
 }
